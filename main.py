b43b52115c6911da7948d54b31fb87e533964530
MuriloZF <mzimermanfortaleza@gmail.com>
2026-03-05 15:06:59 -0300

    Got all the data needed to create the table

--- main.py
+++ main.py
@@ -1,8 +1,23 @@
+import math
+
 erp = []
+size = 208 # Quantidade de valores -1
+def collect():
+    with open("dataset/machine.data", "r") as f:
+        for line in f:
+            columns = line.strip().split(",")
+            erp.append(int(columns[9]))
 
-with open("dataset/machine.data", "r") as f:
-    for line in f:
-        columns = line.strip().split(",")
-        erp.append(int(columns[9]))
 
-print(erp)
+def main():
+    collect()
+    erp.sort() # Passo 1- Rol
+    amplitude = erp[size] - erp[0] # Passo 2- Amplitude
+    classe = math.ceil(1 + 3.33 * math.log10(size)) # Passo 3- Quantidade de classes ( Arredonda para cima)
+    amplitudeClasse = math.ceil(amplitude / classe) # Passo 4- Amplitude de classes (Arredonda para cima)
+    print(erp)
+    print(f'Amplitude: {amplitude}')
+    print(f'Classe: {classe}')
+    print(f'Amplitude Classe: {amplitudeClasse}')
+
+main()
